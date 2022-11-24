bd30075c8ce3f0892ddebaf4d78d6aebca147408
Broizaa <115212246+Broizaa@users.noreply.github.com>
2022-11-24 11:25:41 +0100

    Create carte.hpp

---
+++ poker/carte.hpp
@@ -0,0 +1,17 @@
+#ifndef DEF_CARTE
+#define DEF_CARTE
+
+#include <iostream>
+
+class Carte
+{
+    public:
+    Carte();                    //constructor
+    ~Carte();                   //destructor
+    
+    //private:
+    std::string couleur[];
+    std::string valeur[];
+};
+
+#endif
