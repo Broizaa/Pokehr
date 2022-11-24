0ffcc36d5b63ca7f28389306796ef2ef34c5eecb
Clément Szewczyk <clement.szewczyk@gmail.com>
2022-11-24 22:41:01 +0100

    code qui fonctionne
    manque juste le full et la double paire

---
+++ code_fonc/Carte.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Clément Szewczyk on 25/10/2022.
+//
+
+//#include "Carte.h"
+/*
+Carte::Carte() : m_valeur(4), m_couleur("trefle")
+{
+
+}
+
+Carte::~Carte() {
+
+}*/
+
+
+/*
+char Carte::getCou() const {
+    return cou;
+}
+
+void Carte::setCou(char cou) {
+    Carte::cou = cou;
+}
+
+int Carte::getVal() const {
+    return val;
+}
+
+void Carte::setVal(int val) {
+    Carte::val = val;
+}
+
+Carte::Carte(char cou, int val) : cou(cou), val(val) {}
+*/
+
