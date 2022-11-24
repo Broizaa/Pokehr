e3593634ab2b938ed8584baee989f1e9c4803f1b
Broizaa <115212246+Broizaa@users.noreply.github.com>
2022-11-24 11:26:07 +0100

    Create carte.cpp

---
+++ poker/carte.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "carte.hpp"
+
+Carte::Carte() //: valeur(), couleur()
+{
+    std::string valeur[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Valet", "Dame", "Roi", "As"};
+    std::string couleur[] = {"Trefle", "Coeur", "Carreau", "Pique"};   
+}
+
+Carte::~Carte() {
+    
+}
